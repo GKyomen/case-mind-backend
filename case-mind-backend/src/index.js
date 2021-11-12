3e17afaabe4b2a360b3745ceebfed65b75495255
GKyomen <kenkakyomen@hotmail.com>
2021-11-12 15:33:19 -0300

    added user controller for user authenticated in API wit verification by middleware

--- case-mind-backend/src/index.js
+++ case-mind-backend/src/index.js
@@ -6,5 +6,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 require('./controllers/authController')(app)
+require('./controllers/userController')(app)
 
 app.listen(5000)
---
+++ case-mind-backend/src/middlewares/auth.js
@@ -0,0 +1,37 @@
+const HttpStatus = require('http-status')
+const jwt = require('jsonwebtoken')
+const auth = require('../config/auth.json')
+
+module.exports = function (req, res, next) {
+  const authHeader = req.headers.authorization
+
+  if (!authHeader)
+    return res
+      .status(HttpStatus.UNAUTHORIZED)
+      .send({ error: 'Erro: Nenhum Token informado' })
+
+  const partsToken = authHeader.split(' ')
+
+  if (!partsToken.length === 2)
+    return res
+      .status(HttpStatus.UNAUTHORIZED)
+      .send({ error: 'Erro: Erro no Token' })
+
+  const [part, token] = partsToken
+
+  if (!/^Bearer$/i.test(part))
+    return res
+      .status(HttpStatus.UNAUTHORIZED)
+      .send({ error: 'Erro: Token mal formatado' })
+
+  jwt.verify(token, auth.keySecret, function (err, decodedUser) {
+    if (err)
+      return res
+        .status(HttpStatus.UNAUTHORIZED)
+        .send({ error: 'Erro: Token inválido' })
+
+    req.userId = decodedUser.id
+
+    return next()
+  })
+}
