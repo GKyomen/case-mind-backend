671121251bf62987f880c52462fd8f41abd16023
GKyomen <kenkakyomen@hotmail.com>
2021-11-12 17:33:39 -0300

    get users route added

--- case-mind-backend/src/controllers/userController.js
+++ case-mind-backend/src/controllers/userController.js
@@ -1,11 +1,28 @@
 const express = require('express')
-const authMiddleware = require('../middlewares/auth')
+const HttpStatus = require('http-status')
+const User = require('../models/User')
 const router = express.Router()
+const authMiddleware = require('../middlewares/auth')
 
 router.use(authMiddleware)
 
-router.get('/', (req, res) => {
-  res.send({ ok: true })
+router.get('/users', async (req, res) => {
+  try {
+    const admin = await User.findById(req.userId)
+
+    if (admin.level !== 999)
+      return res.status(HttpStatus.UNAUTHORIZED).send({
+        error: 'Erro: Somente administradores podem ver todos os usuários',
+      })
+
+    const users = await User.find()
+
+    return res.send({ users })
+  } catch (error) {
+    return res
+      .status(HttpStatus.BAD_REQUEST)
+      .send({ error: 'Erro: Não foi possível listar os usuários' })
+  }
 })
 
 module.exports = (app) => app.use('/application', router)
