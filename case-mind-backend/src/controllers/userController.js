40b3c7bcd5fc18b9e5a919bf6aad3f9e54474c2e
GKyomen <kenkakyomen@hotmail.com>
2021-11-12 17:47:15 -0300

    update user route added

--- case-mind-backend/src/controllers/userController.js
+++ case-mind-backend/src/controllers/userController.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const HttpStatus = require('http-status')
 const User = require('../models/User')
+const bcrypt = require('bcrypt')
 const router = express.Router()
 const authMiddleware = require('../middlewares/auth')
 
@@ -25,7 +26,7 @@ router.get('/users', async (req, res) => {
   }
 })
 
-router.get('/:userId', async function (req, res) {
+router.get('/:userId', async (req, res) => {
   try {
     const user = await User.findById(req.params.userId)
 
@@ -34,6 +35,30 @@ router.get('/:userId', async function (req, res) {
     return res
       .status(HttpStatus.BAD_REQUEST)
       .send({ error: 'Erro: Não foi possível mostrar o usuário' })
+  }
+})
+
+router.put('/:userId', async (req, res) => {
+  try {
+    const newData = req.body
+
+    if (newData.password)
+      newData.password = bcrypt.hashSync(req.body.password, 10)
+
+    const userUpdated = await User.findByIdAndUpdate(
+      req.params.userId,
+      newData,
+      {
+        new: true,
+      }
+    )
+
+    await userUpdated.save()
+    return res.send({ userUpdated })
+  } catch (err) {
+    return res
+      .status(HttpStatus.BAD_REQUEST)
+      .send({ error: 'Erro: Não foi possível atualizar o usuário' })
   }
 })
 
