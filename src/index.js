661ea6fa66317279d53e6562e1109b31622872d9
GKyomen <kenkakyomen@hotmail.com>
2021-11-13 14:13:46 -0300

    added CORS to API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(cors())
 
 require('./controllers/authController')(app)
 require('./controllers/userController')(app)
 
-app.listen(5000)
+app.listen(5000, () => {
+  console.log('API running on http://localhost:5000')
+})
